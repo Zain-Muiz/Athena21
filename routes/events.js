e40a337c76166edb877b15831fb438a315c8832b
VI-Suji <sujithvi08@gmail.com>
2021-04-24 18:50:49 +0530

    Deleted contents

--- routes/events.js
+++
@@ -1,8 +0,0 @@
-const express = require ('express');
-const app = express();
-const router = express.Router();
-const eventcontroller = require('../controllers/eventcontroller.js');
-
-router.post('/', eventcontroller.registerevent);
-
-module.exports = router;
