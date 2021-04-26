960111478dd5cec19fcff57d3101491ea40fdca0
V.I_Suji <48118775+VI-Suji@users.noreply.github.com>
2021-04-26 15:46:37 +0530

    Update mail.js

--- controllers/mail.js
+++ controllers/mail.js
@@ -8,7 +8,7 @@ module.exports =(to,text) => {
         let data = {
             from: 'noreply@athena21.live',
             to: to,
-            subject: "Confirm email | Athena'21",
+            subject: 'Confirm email | Athena'21' ,
 //             template: "ISTE_otp",
 //                 'h:X-Mailgun-Variables': JSON.stringify({name: mailData.name, otp: mailData.otp})
             text:text
