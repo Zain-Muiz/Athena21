4330f946a81d8adce80eb4b643149c14c389e317
vishnubvinod0602 <vishnubvinod0602@gmail.com>
2021-05-02 01:45:13 +0530

    some pos

--- routes/pages.js
+++ routes/pages.js
@@ -32,6 +32,9 @@ router.get('/adminlogin', (req,res)=>{
 router.get('/502.html' ,(req,res)=>{
    res.redirect('/home');
 })
+router.get('/thankyou' ,(req,res)=>{
+    res.redirect('/veiws/thankyou.html');
+ })
 
 router.get('/google8bf102bffa1afc17.html', (req,res)=>{
     res.sendFile(path.resolve('views/googled23d6d9f8b681595.html'));
