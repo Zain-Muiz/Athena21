a6ef9a5599a28e7ad581a26975212c1c8c3212aa
VI-Suji <sujithvi08@gmail.com>
2021-05-02 09:35:51 +0530

    logo

--- controllers/paymentcontrol.js
+++ controllers/paymentcontrol.js
@@ -33,6 +33,7 @@ module.exports.amountgenerator = (req,res) =>{
     }
      else{
         discammount = 100;
+        amnt = 200;
         CouponCode = [{name : "AMAP100", amount : discammount},
         {name : "ANEN100",amount : discammount},
         {name : "MUHS100",amount : discammount},
@@ -62,7 +63,7 @@ module.exports.amountgenerator = (req,res) =>{
         {name : "JAYT100",amount : discammount},
         {name : "SALS100" ,amount : discammount},
         {name : "LUCKY100" ,amount : discammount},
-        {name : "EARLYBID10" ,amount : 200}];
+        {name : "EARLYBID10" ,amount : amnt}];
         event1 = req.session.regdetails.event1;
         event2 = req.session.regdetails.event2;
         event3 = req.session.regdetails.event3;
