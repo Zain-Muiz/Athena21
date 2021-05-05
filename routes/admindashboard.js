0b0592c3d761c26548860950fbda7d526801a2aa
Zainuddeen Abdul Muiz <60746360+Zain-Muiz@users.noreply.github.com>
2021-05-05 14:22:14 +0530

    Update admindashboard.js

--- routes/admindashboard.js
+++ routes/admindashboard.js
@@ -3,9 +3,11 @@ const app = express();
 const router = express.Router();
 
 const adminauthcntrl = require('../controllers/adminauthcontrol.js');
+const dbupdate = require('../controllers/dbupdate.js');
 
 
 router.get('/', adminauthcntrl.adminroute);
+router.get('/phupdate', dbupdate.updatenumber);
 
 
 module.exports = router;
