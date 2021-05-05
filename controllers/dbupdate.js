a6bdd98728e8df8a4df79fd998ced16356439e90
Zainuddeen Abdul Muiz <60746360+Zain-Muiz@users.noreply.github.com>
2021-05-05 14:19:34 +0530

    Create dbupdate.js

---
+++ controllers/dbupdate.js
@@ -0,0 +1,26 @@
+
+const bcrypt = require('bcrypt');
+const express = require('express');
+const app = express();
+var db = require('../models/dbconnect');
+var path = require('path');
+app.use(express.static('public'));
+
+
+ module.exports.updatenumber = (req,res) =>{
+                db.query('SELECT * FROM users', (err,newresult) => {
+                    
+                 newresult.forEach(user =>{
+                     db.query("UPDATE registration SET `phNo` = ? WHERE `email` = ?",[user.phNo, user.email], (err,res)=>{
+                         if(err){
+                             console.log(err)
+                         }
+                         else{
+                            console.log("success");
+                         }
+                     })
+                   res.send("DB KERI NOKU");
+                 })
+                
+        
+            })}
