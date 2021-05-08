46321ea80ee4a7b267d5388ddd129e8144920500
V.I_Suji <48118775+VI-Suji@users.noreply.github.com>
2021-05-08 11:24:53 +0530

    Update paymentcontrol.js

--- controllers/paymentcontrol.js
+++ controllers/paymentcontrol.js
@@ -72,7 +72,7 @@ module.exports.amountgenerator = (req,res) =>{
         {name : "Earlybid10" ,amount : 200},
         {name : "ISTE100" ,amount : discammount},
         {name : "ANSH100" ,amount : discammount},
-        {name : "EARLYBID10" ,amount : 200}];
+        {name : "EARLYBID10WINNER" ,amount : 300}];
         event1 = req.session.regdetails.event1;
         event2 = req.session.regdetails.event2;
         event3 = req.session.regdetails.event3;
