8423073e275a7f92732b635663bd598153ffb492
vishnubvinod0602 <vishnubvinod0602@gmail.com>
2021-05-08 21:58:17 +0530

    REGISTRATION STARTED

--- controllers/paymentcontrol.js
+++ controllers/paymentcontrol.js
@@ -72,6 +72,7 @@ module.exports.amountgenerator = (req,res) =>{
         {name : "Earlybid10" ,amount : 200},
         {name : "ISTE100" ,amount : discammount},
         {name : "ANSH100" ,amount : discammount},
+        {name : "TKTU100" ,amount : discammount},
         {name : "EARLYBID10WINNER" ,amount : 300}];
         event1 = req.session.regdetails.event1;
         event2 = req.session.regdetails.event2;
