b25cf19520227473bec7ce355a88ac591eeef859
VI-Suji <sujithvi08@gmail.com>
2021-05-09 16:55:03 +0530

    new event

--- controllers/paymentcontrol.js
+++ controllers/paymentcontrol.js
@@ -74,8 +74,7 @@ module.exports.amountgenerator = (req,res) =>{
         {name : "ISTE100" ,amount : discammount},
         {name : "ANSH100" ,amount : discammount},
         {name : "TKTU100" ,amount : discammount},
-        {name : "EARLYBID10" ,amount : 200},
-        {name : "EARLYBID10WINNER" ,amount : 300}];
+        {name : "EARLYBID10" ,amount : 200}];
         event1 = req.session.regdetails.event1;
         event2 = req.session.regdetails.event2;
         event3 = req.session.regdetails.event3;
