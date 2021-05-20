415a095ac41568cd823be60614bafd519cd998c6
Zain-Muiz <60746360+Zain-Muiz@users.noreply.github.com>
2021-05-21 01:05:56 +0530

    Added mail

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,9 +1,11 @@
 // const nodemailer = require('nodemailer');
 // const mailGun = require('nodemailer-mailgun-transport');
-
+const express = require('express');
+const app = express();
+var db = require('../models/dbconnect');
 const mailgun = require("mailgun-js");
 const mg = mailgun({apiKey: process.env.MAILGUN_NAME, domain: process.env.MAILGUN_HOST});
-module.exports =(email,otp,name) => {
+module.exports.otpmail =(email,otp,name) => {
     return new Promise((resolve, reject)=> {
         let data = {
             from: 'noreply@athena21.live',
@@ -23,6 +25,47 @@ module.exports =(email,otp,name) => {
         }
       });
   });    
+}
+
+
+module.exports.workshopmail =(email,eventname) => {
+    //console.log(email);
+    const dict = {
+        "APP DEVELOPMENT WORKSHOP" : "https://dasfsdfg",
+        "STAAD PRO WORKSHOP" : "https://sdfdsgdfg",
+        "CATIA WORKSHOP" : "https://sdfsdgs",
+        "ELECTRONICS CIRCUIT,PCB DESIGINING WORKSHOP" : "https://sdfasdEQWEASDSD"
+    }
+    //console.log(eventname);
+    link =  dict[`${eventname}`];
+    //console.log(link)
+    return new Promise((resolve, reject)=> {
+                let data = {
+                    from: 'workshops@athena21.live',
+                    to: email,
+                    subject: 'Join Link | Athena21',
+                    template: "athena",
+                    'h:X-Mailgun-Variables': JSON.stringify({ name:"Suji", otp: link, email: email})
+                };
+                mg.messages().send(data, function (error, body) {
+                  if(error) {
+                      console.log(error);
+                    //   logger.error(error);
+                    reject(error);
+                }else {
+                    console.log('Mail sent to', email);
+                    db.query("UPDATE admin SET ? WHERE email = ? ", [{isclick: "1"}, email], (err, result) =>{
+                        if(err){
+                            console.log(err);
+                        }
+                        else{
+                            resolve('ES');
+                        }
+                    })
+                   
+                }
+              });
+          });    
 }
 
 // myfunc();
